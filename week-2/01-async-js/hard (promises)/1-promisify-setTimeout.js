a1e03e6acb95c303701b0d2c5486dcec99fa7b3e
murari2401 <iammurari89@gmail.com>
2024-05-08 01:39:07 +0530

    week2-async-hard-done

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,16 @@
 */
 
 function wait(n) {
+    return new Promise((resolve,reject)=>{
+        setTimeout(resolve,n*1000);
+    })
 }
+
+// wait(5).then((result)=>{
+//     console.log(result);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
 
 module.exports = wait;
